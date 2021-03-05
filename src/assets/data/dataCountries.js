9958dea629d9b45ec2493bb3eee7aa1e4f0749c0
Adelheid483 <adelheid0106@gmail.com>
2021-03-06 01:11:27 +0300

    init: start make data

---
+++ src/assets/data/dataCountries.js
@@ -0,0 +1,95 @@
+export const dataCountries = [
+  {
+    country: {
+      country_eng: 'Australia',
+      country_rus: 'Австралия',
+      country_bel: 'Аўстралія',
+    },
+
+    capital: {
+      capital_eng: 'Canberra',
+      capital_rus: 'Канберра',
+      capital_bel: 'Канбера',
+      coordinates: {
+        lat: '-35.282001',
+        long: '149.128998',
+      },
+    },
+
+    iso: 'ISO 3166-2:AU',
+
+    picture: '',
+
+    description: {
+      description_eng: 'Australia is the world\'s sixth-largest country by land area, and it is the only nation to occupy an entire continent. The Commonwealth of Australia consists of the Australian mainland and several islands, the largest of which is Tasmania. On the mainland, a diverse natural environment coexists with modern, densely populated metropolitan areas. While much of the continent is desert and semi-desert, Australia\'s landscapes range from alpine grasslands to tropical jungles. Australia is home to unique species of flora and fauna, some of which are not found elsewhere in the world. Many plants and animals, including giant marsupials, became extinct with the arrival of the natives; others (such as the Tasmanian tiger) became extinct with the arrival of Europeans.\n' +
+        'Australia is a cloudless blue sky, bright sunshine, miles of white sand beaches and ocean to the horizon. On Australia\'s north-east coast stretches the Great Barrier Reef, home to the World Heritage-listed, unique marine national park. \n' +
+        'Australia\'s attraction lies not just in the nature of the mainland. Welcoming cities, cultural and business centers contribute to its attractions. In every major city, be it Sydney, Canberra, Melbourne or any other major city, historic sites rub shoulders with skyscrapers, cosy parks with busy streets and varied museums with chic stores.\n',
+      description_rus: 'Австралия – шестая в мире страна по размеру территории, и это единственное государство, занимающее целый континент. Австралийский союз включает в себя Австралийский материк и несколько островов, крупнейшим из которых является Тасмания. На территории материка разнообразная природа соседствует с современными густонаселенными мегаполисами. Хотя большую часть континента занимают полупустыни и пустыни, в Австралии имеются разнообразные ландшафты: – от альпийских лугов до тропических джунглей. Австралия стала домом для уникальных видов флоры и фауны, некоторые из которых не встречаются в других уголках планеты. Многие растения и животные, включая гигантских сумчатых, вымерли с появлением аборигенов; другие (например, тасманский тигр) – с появлением европейцев.\n' +
+        'Австралия – это безоблачное синее небо, яркое солнце, многокилометровые пляжи с белоснежным песком и океан до горизонта. Вдоль северо-восточного побережья Австралии протянулся Большой Барьерный риф, где расположен уникальный морской национальный парк, внесенный в список Всемирного наследия. \n' +
+        'Привлекательность Австралии заключена не только в природе материка. Вносят сюда свою лепту и благоустроенные города, центры культурной и деловой жизни государства. Во всех мегаполисах – будь то Сидней, Канберра, Мельбурн или любой другой крупный город, – исторические достопримечательности соседствуют с небоскребами, уютные парки – с многолюдными улицами, а разнообразные музеи – с шикарными магазинами.\n',
+      description_bel: 'Аўстралія - шостая ў свеце краіна па памеры тэрыторыі, і гэта адзіная дзяржава, якая займае цэлы кантынент. Аўстралійскі Саюз ўключае ў сябе Аўстралійскі мацярык і некалькі выспаў, найбуйнейшым з якіх З\'яўляецца Тасманія. На тэрыторыі мацерыка разнастайная прырода суседнічае з сучаснымі густанаселенымі мегаполісамі. Хоць большую частку кантынента займаюць паўпустыні і пустыні, у Аўстраліі маюцца разнастайныя ландшафты: – ад альпійскіх лугоў да трапічных джунгляў. Аўстралія стала домам для унікальных відаў флоры і фауны, некаторыя з якіх не сустракаюцца ў іншых кутках планеты. Многія расліны і жывёлы, у тым ліку гіганцкіх сумчатых, вымерлі з з\'яўленнем абарыгенаў; іншыя (напрыклад, тасманскага тыгр) – з з\'яўленнем еўрапейцаў.\n' +
+        'Аўстралія-гэта бясхмарнае сіняе неба, яркае сонца, шматкіламетровыя пляжы з беласнежным пяском і акіян да гарызонту. Уздоўж паўночна-ўсходняга ўзбярэжжа Аўстраліі працягнуўся Вялікі Бар\'ерны рыф, дзе размешчаны унікальны марскі Нацыянальны парк, унесены ў спіс Сусветнай спадчыны.\n' +
+        'Прывабнасць Аўстраліі складзена не толькі ў прыродзе мацерыка. Ўносяць сюды сваю лепту і добраўпарадкаваныя гарады, цэнтры культурнай і дзелавой жыцця дзяржавы. Ва ўсіх мегаполісах-няхай гэта будзе Сіднэй, Канбера, Мэльбурн або любы іншы буйны горад, – гістарычныя славутасці суседнічаюць з хмарачосамі, утульныя паркі – з шматлюднымі вуліцамі, а разнастайныя музеі - з шыкоўнымі крамамі.\n',
+    },
+
+    sight_1: {
+      name_eng: 'The Great Barrier Reef',
+      name_rus: 'Большой Барьерный риф',
+      name_bel: 'Вялікі Бар\'ерны рыф',
+      info_eng: 'The Great Barrier Reef, a huge range of coral reefs and islands, stretches along Australia\'s northwestern coastline. The Great Reef or National Reef Park, a UNESCO World Heritage Site, is truly enormous: it is about 10% of the world\'s coral reefs. Modern studies show that the first reefs here began to form about 18 million years ago. During this time, the old reefs were destroyed. and in their place grew new ones. Today, only a small part of the coral is visible on the surface of the sea, and only a scuba dive will allow you to contemplate the most complete and picturesque picture.',
+      info_rus: 'Вдоль побережья Австралии на северо-западе протянулся Большой Барьерный риф, представляющий собой огромную гряду коралловых рифов и островов. Большой риф или Национальный парк рифов, внесённый в список Всемирного наследия ЮНЕСКО, поистине огромен: его площадь составляет около 10% от площади всех коралловых рифов на планете. Современные исследования показывают, что первые рифы здесь стали образовываться примерно 18 млн лет назад. За это время старые рифы разрушались. а на  их месте вырастали новые. На сегодняшний день на поверхности морской глади видна лишь небольшая часть кораллов, и лишь подводное плавание позволит созерцать наиболее полную и живописную картину.',
+      info_bel: 'Уздоўж ўзбярэжжа Аўстраліі на паўночна-захадзе працягнуўся Вялікі Бар\'ерны рыф, які ўяўляе сабой вялікую граду каралавых рыфаў і выспаў. Вялікі рыф або нацыянальны парк рыфаў, унесены ў спіс Сусветнай спадчыны ЮНЕСКА, сапраўды велізарны: яго плошча складае каля 10% ад плошчы ўсіх каралавых рыфаў на планеце. Сучасныя даследаванні паказваюць, што першыя рыфы тут сталі ўтварацца прыкладна 18 млн гадоў таму. За гэты час старыя рыфы разбураліся. а на іх месцы вырасталі новыя. На сённяшні дзень на паверхні марской роўнядзі бачная толькі невялікая частка каралаў, і толькі Падводнае плаванне дазволіць сузіраць найбольш поўную і жывапісную карціну.',
+      img: '',
+    },
+
+    sight_2: {
+      name_eng: 'Royal Canyon',
+      name_rus: 'Королевский каньон',
+      name_bel: 'Каралеўскі Каньён',
+      info_eng: 'Located in Australia\'s Red Centre, Royal Canyon was once sacred land to local Aboriginal people. Today it has achieved the status of an Australian landmark because of its unique appearance and peculiar natural beauty. Formed of sandstone, the steep canyon walls, up to 200 meters high, have been formed over millions of years, acquiring unusual shapes under the influence of wind erosion. Inside the canyon you\'ll find dense thickets of ferns, cycads, palm forests, and hundreds of animal species-all thanks to the canyon walls, which protect local flora and fauna from the desert drought that prevails outside the canyon.',
+      info_rus: 'Расположенный в Красном центре Австралии Королевский каньон некогда имел статус священной земли для местных аборигенов. Сегодня он обрёл статус достопримечательности Австралии, благодаря своему уникальному виду и своеобразной природной красоте. Сформированные из песчаника отвесные стены каньона, достигающие 200 метров в высоту, формировались миллионы лет, приобретая необычные формы под воздействием ветровой эрозии. Внутри каньона произрастают густые заросли папоротников и саговников, пальмовые леса, находят приют сотни видов животных — всё это благодаря стенам каньона, которые для местной флоры и фауны играют роль защиты от пустынной засухи, царящей за пределами каньона.',
+      info_bel: 'Размешчаны ў чырвоным цэнтры Аўстраліі Каралеўскі Каньён некалі меў статус свяшчэннай зямлі для мясцовых абарыгенаў. Сёння ён набыў статус славутасці Аўстраліі, дзякуючы свайму ўнікальнаму ўвазе і своеасаблівай прыроднай прыгажосці. Сфармаваныя з пяшчаніку стромыя сцены каньёна, якія дасягаюць 200 метраў у вышыню, фармаваліся мільёны гадоў, набываючы незвычайныя формы пад уздзеяннем ветравой эрозіі. Унутры каньёна растуць густыя зараснікі папараці і сагаўнікоў, пальмавыя лясы, знаходзяць прытулак сотні відаў жывёл-усё гэта дзякуючы сценам каньёна, якія для мясцовай флоры і фауны гуляюць ролю абароны ад пустыннай засухі, панавальнай за межамі каньёна.',
+      img: '',
+    },
+
+    sight_3: {
+      name_eng: 'Cuisine',
+      name_rus: 'Кухня',
+      name_bel: 'Кухня',
+      info_eng: 'Australian cuisine is not distinctive, but rather a combination of the different gastronomies of the world. Vegemite is the most Australian of all Australian dishes. In 1922 Australian Fred Volker "invented" a special "yeast extract" that was equally nutritious, and delicious. The extract was mixed with celery, onions and salt. The result was a thick, dark mass that could be spread on bread or consumed as a prepared meal.',
+      info_rus: 'Австралийская кухня не самобытна, а скорее является сочетанием разных гастрономий мира. Vegemite – самое австралийское из всех австралийских блюд. В 1922 году австралиец Фред Волкер “придумал” специальный “дрожжевой экстракт”, который был одинаково питательным, и вкусным. Экстракт был смешан с сельдереем, луком и солью. Получилась густая темная масса, которую можно было намазывать на хлеб или употреблять в качестве готового блюда.',
+      info_bel: 'Аўстралійская кухня не самабытная, а хутчэй з\'яўляецца спалучэннем розных гастранамій свету. Vegemite - самае аўстралійскае з усіх аўстралійскіх страў. У 1922 годзе аўстраліец Фрэд Волкер "прыдумаў" спецыяльны "дражджавы экстракт", які быў аднолькава пажыўным, і смачным. Экстракт быў змяшаны з салерай, цыбуляй і соллю. Атрымалася густая цёмная маса, якую можна было нашмароўваць на хлеб ці ўжываць у якасці гатовага стравы.',
+      img: '',
+    },
+
+    sight_4: {
+      name_eng: 'Festival',
+      name_rus: 'Фестиваль',
+      name_bel: 'Фестываль',
+      info_eng: 'The traditional ATP Austrian Open tennis tournament is held in Melbourne in late January. The Australian Grand Prix of the Formula One World Championship is held annually in Melbourne in February and March. "Austrian International Airshow" is a world famous airshow held annually in February-March at Avalon airport, a suburb of Melbourne - Geelong. The Darwin Beer Regatta stands out among the sporting events in August, when there is a series of "watercraft" races built entirely out of beer cans and barrels. On Thanksgiving Eve (December), there\'s the Sydney to Hobart International Yacht Race, and the August 26 Yacht Race around Australia starts. And almost all life in the country comes to a standstill during the Melbourne Cup (first Tuesday in November).',
+      info_rus: 'Традиционный теннисный турнир ATP "Оустрэлиэн Оупен" проводится в Мельбурне в конце января. Австралийское Гран При мирового чемпионата в гонках класса Формула-1 ежегодно проходит в феврале-марте в Мельбурне. "Оустрэлиэн Интернешнл Эйршоу" - всемирно известный авиасалон, который проводится ежегодно в феврале - марте в аэропорту Авалон предместья Мельбурна - Джилонга. Среди спортивных соревнований выделяется Пивная Регата Дарвина в августе, когда проходит целый ряд гонок "плавсредств", построенных полностью из пивных банок и бочек. В канун Дня Благодарения (декабрь) проходит международная яхтенная гонка Сидней - Хобарт, а 26 августа стартует яхтенная регата вокруг Австралии. И практически вся жизнь в стране замирает в период проведения Кубка Мельбурна по футболу (первый вторник ноября).',
+      info_bel: 'Традыцыйны тэнісны турнір ATP "Оўстрэліэн оўпэн" праводзіцца ў Мельбурне ў канцы студзеня. Аўстралійскае Гран пры сусветнага чэмпіянату ў гонках класа Формула - 1 штогод праходзіць у лютым-сакавіку ў Мельбурне. "Оўстрэліэн Інтэрнэшнл Эйршоў" - сусветна вядомы авіясалон, які праводзіцца штогод у лютым - сакавіку ў аэрапорце Авалон прадмесця Мельбурна-Джылонга. Сярод спартыўных спаборніцтваў вылучаецца піўная Рэгата Дарвіна ў жніўні, калі праходзіць цэлы шэраг гонак "плаўсродкаў", пабудаваных цалкам з піўных слоікаў і бочак. Напярэдадні Дня Падзякі (снежань) праходзіць міжнародная яхтенных гонка Сіднэй-Хобарт, а 26 жніўня стартуе яхтенных рэгата вакол Аўстраліі. І практычна ўсё жыццё ў краіне замірае ў перыяд правядзення Кубка Мельбурна па футболе (першы аўторак лістапада).',
+      img: '',
+    },
+
+    sight_5: {
+      name_eng: 'Opera House',
+      name_rus: 'Оперный театр',
+      name_bel: 'Оперны тэтр',
+      info_eng: 'The landscape of Sydney is unmistakably recognizable among thousands of other cities thanks to the world famous landmark - the Opera House Opera House building, one of the most famous in the world architecture. This unique building was designed by a Danish architect named Jorn Utzon. By designing the roofs of the Opera House, which are original in form, the Dane gave Sydney a symbol of the city, by which almost every inhabitant of the planet recognizes it now. Many years after the construction of the famous building, the architect admitted that he was inspired to create the unique roof of the theater... by an orange peel taken from the fruit in triangular segments.',
+      info_rus: 'Пейзаж Сиднея однозначно узнаётся среди тысяч других городов благодаря известной во всём мире достопримечательности — зданию оперного театра Опера-хаус, одному из самых знаменитых в мировой архитектуре. Это уникальное здание было спроектировано датским архитектором по имени Йорн Утзон. Спроектировав оригинальные по форме крыши театра, датчанин подарил Сиднею символ города, по которому теперь чуть ли не каждый житель планеты узнает этот город. Спустя много лет с момента возведения знаменитого здания архитектор признался, что на создание уникальной крыши театра его вдохновила… корка апельсина, снятая с фрукта треугольными сегментами.',
+      info_bel: 'Пейзаж Сіднэя адназначна пазнаецца сярод тысяч іншых гарадоў дзякуючы вядомай ва ўсім свеце славутасці-будынку Опернага тэатра Опера-Хаўс, аднаму з самых знакамітых у сусветнай архітэктуры. Гэта унікальнае будынак было спраектавана дацкім архітэктарам па імя Йорн Утзон. Спраектаваўшы арыгінальныя па форме даху тэатра, датчанін падарыў Сіднэя сімвал горада, па якім цяпер ледзь ці не кожны жыхар планеты пазнае гэты горад. Праз шмат гадоў з моманту ўзвядзення знакамітага будынка архітэктар прызнаўся, што на стварэнне унікальнай даху тэатра яго натхніла... скарынка апельсіна, знятая з фрукта трохкутнымі сегментамі.',
+      img: '',
+    },
+
+    sight_6: {
+      name_eng: 'Harbor Bridge',
+      name_rus: 'Мост Харбор-Бридж',
+      name_bel: 'Мост Харбар-Брыдж',
+      info_eng: 'Sydney\'s Harbor Bridge is the largest steel arch bridge in the world. It is often called the "hanger" by locals, because of its similarity to this object. The grand opening of the bridge took place in 1932, although construction was planned back in the mid-19th century. Harbor Bridge crosses Port Jackson Bay, connecting the North Shore and the business part of the city. The bridge can be used by pedestrians and cyclists, as well as cars - there are special roads for all. The bridge is 1,150 meters long and 134 meters high, and those who wish can climb to its highest point to explore beautiful city panoramas.',
+      info_rus: 'Сиднейский мост Харбор-Бридж — крупнейший арочный мост в мире, возведённый из стали. Нередко местные его называют «вешалкой», по причине схожести конструкции с этим предметом. Торжественное открытие моста состоялось в 1932 году, хотя строительство было запланировано ещё в середине 19 века. Харбор-Бридж пересекает залив Порт-Джексон, соединяя Северный берег и деловую часть города. Воспользоваться мостом могут как пешеходы и велосипедисты, так и машины — для всех предусмотрены специальные дороги. Мост имеет длину 1150 метров, высоту 134 метра, и желающие могут взобраться на самую его верхнюю точку для изучения прекрасных городских панорам.',
+      info_bel: 'Сіднейскі мост Харбар-Брыдж-найбуйнейшы арачны мост у свеце, збудаваны з сталі. Нярэдка мясцовыя яго называюць "вешалкай", па прычыне падабенства канструкцыі з гэтым прадметам. Урачыстае адкрыццё моста адбылося ў 1932 годзе, хоць будаўніцтва было запланавана яшчэ ў сярэдзіне 19 стагоддзя. Харбар-Брыдж перасякае заліў Порт-Джэксан, злучаючы Паўночны бераг і дзелавую частку горада. Скарыстацца мостам могуць як пешаходы і веласіпедысты, так і машыны - для ўсіх прадугледжаны спецыяльныя дарогі. Мост мае даўжыню 1150 метраў, вышыню 134 метра, і жадаючыя могуць ўзлезці на самую яго верхнюю кропку для вывучэння выдатных гарадскіх панарам.',
+      img: '',
+    },
+  }
+];
