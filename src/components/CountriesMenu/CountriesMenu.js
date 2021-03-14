f5ec5ac6d8e2da4e085d8d5461daf592a6b8a3ec
ZhenjaHorbach <HorbachZhenja@yandex.by>
2021-03-14 19:39:54 +0300

    fix: clear error

--- src/components/CountriesMenu/CountriesMenu.js
+++ src/components/CountriesMenu/CountriesMenu.js
@@ -14,65 +14,6 @@ export default class CountriesMenu extends Component {
   render() {
     const { onCountryChange, countriesInfo, language } = this.props;
 
-    const countriesInfoTest = [
-      {
-        id: 1,
-        country: "France",
-        capital: "name",
-        cover: "images/temp.jpg",
-        iso: "FRA",
-      },
-      {
-        id: 2,
-        country: "The Netherlands",
-        capital: "Amsterdam",
-        cover: "images/temp.jpg",
-        iso: "NLD",
-      },
-      {
-        id: 3,
-        country: "Germany",
-        capital: "Berlin",
-        cover: "images/temp.jpg",
-        iso: "DEU",
-      },
-      {
-        id: 4,
-        country: "Australia",
-        capital: "Canberra",
-        cover: "images/temp.jpg",
-        iso: "AUS",
-      },
-      {
-        id: 5,
-        country: "Morocco",
-        capital: "Rabat",
-        cover: "images/temp.jpg",
-        iso: "MAR",
-      },
-      {
-        id: 6,
-        country: "Japan",
-        capital: "Tokyo",
-        cover: "images/temp.jpg",
-        iso: "JPN",
-      },
-      {
-        id: 7,
-        country: "Brazil",
-        capital: "Brasília",
-        cover: "images/temp.jpg",
-        iso: "BRA",
-      },
-      {
-        id: 8,
-        country: "Canada",
-        capital: "Ottawa",
-        cover: "images/temp.jpg",
-        iso: "CAN",
-      },
-    ];
-
     const allCards = countriesInfo.map(
       ({ id, country, capital, cover, iso }) => {
         return (
